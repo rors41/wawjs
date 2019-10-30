6292125b790689775152fb4849032eca941de14d
Matej Martinkovič <martinkovic.mat@gmail.com>
2019-10-30 11:39:04 +0100

    damage control

--- 06-cvicenie/ulohy/src/saveSomewhere1.js
+++
@@ -1,8 +0,0 @@
- const async = require("async");
- const fs = require("fs");
- module.exports = saveSomewhere;
-
- function saveSomewhere(paths, data, cb) {
-   const tasks = //...
-   async./*whatMethod*/(tasks, cb);
- }
--- 06-cvicenie/ulohy/src/saveSomewhere2.js
+++
@@ -1,7 +0,0 @@
- const async = require("async");
- const fs = require("fs");
- module.exports = saveSomewhere;
-
- function saveSomewhere(paths, data, cb) {
-   //....
- }
