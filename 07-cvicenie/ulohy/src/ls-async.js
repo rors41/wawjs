f4cd2947961985c7664761607f26fe04683cdf4a
Matej Martinkovič <martinkovic.mat@gmail.com>
2019-11-07 14:17:36 +0100

    cviko7

--- 07-cvicenie/ulohy/src/ls-async.js
+++ 07-cvicenie/ulohy/src/ls-async.js
@@ -3,22 +3,36 @@ const path = require("path")
 
 module.exports = lsRescursive
 
-function lsRescursive(dirName) {
+// function lsRescursive(dirName) {
 
-  return ls(dirName)
-    .then(dirsOnly)
-    .then(dirs => dirs.map(({ name }) => name))
-    .then(dirs => dirs.map(name => path.resolve(dirName, name)))
-    .then(dirs => dirs.map(ls)) // [] of Promises of []s
-    .then(files => Promise.all(files)) // Promise of [] of []s
-    .then(files => [].concat(...files)) // [[],[],...]-> [.,.,.]
-    .then(filesOnly)
-    .then((files) =>
-      files.map(({ name }) => name)
-    )
+//   return ls(dirName)
+//     .then(dirsOnly)
+//     .then(dirs => dirs.map(({ name }) => name))
+//     .then(dirs => dirs.map(name => path.resolve(dirName, name)))
+//     .then(dirs => dirs.map(ls)) // [] of Promises of []s
+//     .then(files => Promise.all(files)) // Promise of [] of []s
+//     .then(files => [].concat(...files)) // [[],[],...]-> [.,.,.]
+//     .then(filesOnly)
+//     .then((files) =>
+//       files.map(({ name }) => name)
+//     )
+// }
+
+
+async function lsRescursive(dirName){
+  let dirs = await ls(dirName);
+  dirs = dirsOnly(dirs);
+  dirs = dirs.map(({name}) => name);
+  dirs = dirs.map(name => path.resolve(dirName, name));
+  let files = dirs.map(ls);
+  files = await Promise.all(files);
+  files = files.flat();
+  let fOnly = filesOnly(files);
+  return fOnly.map(({name}) => name);   
 }
 
-function ls(dirName) {
+
+async function ls(dirName) {
   return fs.readdir(dirName, {
     withFileTypes: true
   });
@@ -31,4 +45,6 @@ function dirsOnly(files) {
 function filesOnly(files) {
   return files.filter((f) => f.isFile());
 }
+
+
 
